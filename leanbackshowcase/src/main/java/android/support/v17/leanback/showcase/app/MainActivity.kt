975e4962ddda31f0fc16df7839570e2cc1ff5f41
niupengjie <2948023335@qq.com>
2019-02-27 11:35:28 +0800

    添加fea-leanback用于研究v17.leanback框架

---
+++ leanbackshowcase/src/main/java/android/support/v17/leanback/showcase/app/MainActivity.kt
@@ -0,0 +1,16 @@
+package android.support.v17.leanback.showcase.app
+
+import android.app.Activity
+import android.os.Bundle
+
+/**
+ * @author Jacob Niu
+ * @since 2019-02-26
+ */
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+}
